d7f6b2e91a1bca31e87762cf27f0c0ad078e36af
שרון דיסקין <sharondi@srwns-MacBook-Pro.local>
2023-12-19 13:57:26 +0200

    WIP

--- .github/scripts/helm_dry_run.sh
+++ .github/scripts/helm_dry_run.sh
@@ -7,22 +7,22 @@ if [ "$#" -ne 2 ]; then
 fi
 
 # Assign the input arguments to variables
-HELM_CHART_PATH="$1"
-HELM_VALUES_FILE_PATH="$2"
+CHART_NAME="$1"
+HELM_CHART_PATH="$2"
+HELM_VALUES_FILE_PATH="$3"
 
-# Main script logic
 if [ -f "$HELM_VALUES_FILE_PATH" ]; then
     DIR_NAME=$(dirname "$HELM_VALUES_FILE_PATH")
 
     echo "==================== Dry run Helm Chart with $(basename "$DIR_NAME") values ===================="
-    helm install --dry-run --debug test-release $HELM_CHART_PATH -f "$HELM_VALUES_FILE_PATH"
+    helm install --dry-run --debug $CHART_NAME $HELM_CHART_PATH -f "$HELM_VALUES_FILE_PATH"
     if [ $? -ne 0 ]; then
       echo "Dry run test failed for $(basename "$DIR_NAME")"
       exit 1
     fi
 
     echo "==================== Helm Diff for $(basename "$DIR_NAME") ===================="
-    helm diff upgrade --allow-unreleased test-release $HELM_CHART_PATH -f "$HELM_VALUES_FILE_PATH"
+    helm diff upgrade --allow-unreleased $CHART_NAME $HELM_CHART_PATH -f "$HELM_VALUES_FILE_PATH"
     if [ $? -ne 0 ]; then
       echo "Check diff failed for $(basename "$DIR_NAME")"
       exit 1
